82b4863ac8650d5fb34abec16775d981c044b629
Chris de Graaf <chrisadegraaf@gmail.com>
2019-04-07 12:56:00 -0500

    More dispatchy code

--- src/JLisp.jl
+++ src/JLisp.jl
@@ -2,34 +2,30 @@ module JLisp
 
 export @jlisp
 
-const V = Vector
-
-# https://unicode-table.com/en/275C/
+# U+275C.
 const ESC = :❜  # TODO: Find a better symbol.
 
 isescape(x) = false
-isescape(ex::Expr) = ex.head === :call && length(ex.args) === 3 && ex.args[2] === ESC
-unescape(arg) = isescape(arg) ? [first(arg.args), last(arg.args)] : [arg]
-unescape(args::V) = mapreduce(unescape, vcat, args)
+isescape(ex::Expr) = ex.head === :call && length(ex.args) == 3 && ex.args[2] === ESC
 
-"""
-A JLisp syntax error.
-`msg` contains an error message, and `x` contains what caused it.
-"""
+unescape(arg) = isescape(arg) ? [first(arg.args), last(arg.args)] : [arg]
+unescape(args::Tuple) = mapreduce(unescape, vcat, args; init=Any[])
+unescape(args...) = unescape(args)
+
 struct SyntaxError <: Exception
     msg::String
-    x
 end
 
-function Base.showerror(io::IO, e::SyntaxError)
-    print(io, "SyntaxError: $(e.msg)\nValue: ")
-    dump(io, e.x; maxdepth=1)
+Base.showerror(io::IO, e::SyntaxError) = print(io, "Syntax error: ", e.msg)
+
+macro se_str(s)
+    :(throw(SyntaxError($s)))
 end
 
 """
     @jlisp {your {sexp here}}
 
-Parse and execute some JLisp code.
+Parse and execute some jlisp code.
 """
 macro jlisp(ex)
     esc(sexp(ex))
@@ -37,149 +33,122 @@ end
 
 # Parses a file or literal code.
 parse(io::IO) = parse(read(io, String))
-function parse(code::String)
-    exs = []
+function parse(code::AbstractString)
+    block = Expr(:block)
     start = 1
     while true
         ex, next = Meta.parse(code, start)
         start == next && break
         start = next
-        push!(exs, sexp(ex))
+        push!(block.args, @show(sexp(@show ex)))
     end
-    exs
+    block
 end
 
 # Runs some parsed code.
-run(exs::Vector) = foreach(ex -> Core.eval(@__MODULE__, ex), exs)
+run(exs...) = foreach(ex -> Core.eval(@__MODULE__, ex), exs)
 
 # This catches literals like symbols, numbers, and strings.
 sexp(x) = x
 
 # This is the entrypoint for expressions.
 # They get split into their head and arguments for dispatch purposes.
-sexp(ex::Expr) = sexp(Val(ex.head), ex.args)
+sexp(ex::Expr) = sexp(Val(ex.head), ex.args...)
 
 # This catches any expression head that we don't explicitly handle.
-sexp(::Val{H}, args::V) where H = Expr(H, map(sexp, args)...)
+sexp(::Val{H}, args...) where H = Expr(H, map(sexp, args)...)
 
 # The only valid :braces expressions are {} and {fun}.
-function sexp(::Val{:braces}, args::V)
-    if length(args) > 1
-        msg = "Commas are not allowed inside expressions"
-        throw(SyntaxError(msg, Expr(:braces, args...)))
-    elseif length(args) == 1
-        Expr(:call, sexp(first(args)))
-    else
-        :nothing
-    end
-end
+sexp(::Val{:braces}) = :nothing
+sexp(::Val{:braces}, arg1) = Expr(:call, sexp(arg1))
+sexp(::Val{:braces}, args...) = se"Commas are not allowed inside expressions"
 
 # These are your generic S-expressions.
 # They're really just containers for a :row representing a :call or keyword expression,
 # and optionally another :row representing keyword arguemnts to a :call.
-function sexp(::Val{:bracescat}, args::V)
-    if length(args) > 2
-        msg = "Only one semicolon is allowed to indicate keyword arguments"
-        throw(SyntaxError(msg, Expr(:bracescat, args...)))
-    elseif !all(ex -> ex isa Expr && ex.head === :row, args)
-        msg = "Unrecognized :bracescat expression argument"
-        throw(SyntaxError(msg, Expr(:bracescat, args...)))
-    end
-    ex = sexp(first(args))
-    kws = kwlist(length(args) == 2 ? last(args) : Expr(:row))
+function sexp(::Val{:bracescat}, arg1::Expr, arg2::Expr=Expr(:row))
+    arg1.head === :row || arg2.head === :row || se"Unrecognized expression syntax"
+    ex = sexp(arg1)
+    kws = kwlist(arg2)
     isempty(kws) || insert!(ex.args, 2, Expr(:parameters, kws...))
     ex
 end
+sexp(::Val{:bracescat}, arg1, arg2, arg3, args...) =
+    se"Only one semicolon is allowed to add keyword arguments"
+sexp(::Val{:bracescat}, args...) = se"Unrecognized expression argument"
 
 # These usually usually represent function calls, e.g. {max 1 2}.
 # However, keyword arguments to the call are missing,
 # since they are found in the containing :bracescat expression.
 # The first argument can also be a keyword, e.g. {:if {> {rand} 0.5} 1 2}.
-function sexp(::Val{:row}, args::V)
-    args = unescape(args)
-    hd, args = peel(args)
-    if hd isa QuoteNode
-        # Keyword expression.
-        kwexpr(Val(hd.value), args)
-    else
-        # In very simple cases, avoid having to use the escape operator.
-        infix_arg1 = nothing
-        if hd isa Expr && hd.head === :call
-            length(hd.args) == 2 ||
-                throw(SyntaxError("Unrecognized function call syntax", Expr(:row, args...)))
-            hd, infix_arg1 = hd.args
-        end
-        ex = Expr(:call, hd, map(sexp, args)...)
-        infix_arg1 === nothing || insert!(ex.args, 2, sexp(infix_arg1))
-        ex
+sexp(::Val{:row}, arg1::QuoteNode, args...) = kwexpr(Val(arg1.value), unescape(args)...)
+function sexp(::Val{:row}, arg1, args...)
+    args = unescape(arg1, args...)
+    arg1, args = first(args), args[2:end]
+    # In very simple cases, avoid having to use the escape operator.
+    infix_arg1 = nothing
+    if arg1 isa Expr && arg1.head === :call
+        length(arg1.args) == 2 || se"Unrecognized function call syntax"
+        arg1, infix_arg1 = arg1.args
     end
+    ex = Expr(:call, arg1, map(sexp, args)...)
+    infix_arg1 === nothing || insert!(ex.args, 2, sexp(infix_arg1))
+    ex
 end
 
 # Converts a :row of keyword arguments into a list of :kw expressions.
-kwlist(x) = throw(SyntaxError("Invalid value as keyword arguments", x))
+kwlist(x) = se"Invalid value as keyword arguments"
 function kwlist(ex::Expr)
     if ex.head === :(=)
         # Single keyword.
         first(ex.args) isa Symbol ||
-            throw(SyntaxError("Keyword argument name must be an identifier", ex))
+            se"Keyword argument name must be an identifier"
         Expr(:kw, first(ex.args), sexp(last(ex.args)))
     elseif ex.head === :row
         # Keyword list.
         map(kwlist, ex.args)
     else
-        throw(SyntaxError("Unrecognized keyword argument syntax", ex))
+        se"Unrecognized keyword argument syntax"
     end
 end
 
 # An assignment, i.e. var = val.
 # Example: {:def x 1}.
-function kwexpr(::Val{:def}, args::V)
-    ex = Expr(:row, :(:def), args...)
-    length(args) == 2 || throw(SyntaxError("Wrong number of arguments to :def", ex))
-    first(args) isa Symbol ||
-        throw(SyntaxError("First argument to :def must be an identifier", ex))
-    Expr(:(=), first(args), sexp(last(args)))
-end
+kwexpr(::Val{:def}, arg1::Symbol, arg2) = Expr(:(=), arg1, sexp(arg2))
+kwexpr(::Val{:def}, arg1, arg2) = se"First argument to :def must be an identifier"
+kwexpr(::Val{:def}, args...) = se"Wrong number of arguments to :def"
 
 # An if-else expression that does not contain any elseif.
-# Example: {:if {fun args} truebody falsebody}
-function kwexpr(::Val{:if}, args::V)
-    ex = Expr(:row, :(:if), args...)
-    length(args) < 2 && throw(SyntaxError("Not enough arguments to :if", ex))
-    length(args) > 4 &&
-        throw(SyntaxError("Too many arguments to :if (use :cond for elseif)", ex))
-    Expr(:if, map(sexp, args)...)
-end
+# Example: {:if {fun args} {do this} {otherwise this}}
+kwexpr(::Val{:if}, arg1, arg2, arg3=:nothing) =
+    Expr(:if, sexp(arg1), sexp(arg2), sexp(arg3))
+kwexpr(::Val{:if}, arg1, arg2, arg3, arg4, args...) =
+    se"Too many arguments to :if (use :cond for elseif)"
+kwexpr(::Val{:if}, args...) = se"Not enough arguments to :if"
 
 # A for loop.
 # Example: {:for {i 1:10} {print i}}
-function kwexpr(::Val{:for}, args::V)
-    ex = Expr(:row, :(:for), args...)
-    length(args) == 2 || throw(SyntaxError("Wrong number of arguments to :for", ex))
-    it = sexp(first(args))
+function kwexpr(::Val{:for}, arg1::Expr, arg2=:nothing)
+    it = sexp(arg1)
     it isa Expr && it.head === :call && length(it.args) == 2 && first(it.args) isa Symbol ||
-        throw(SyntaxError("Invalid iterator specification in :for", ex))
-    Expr(:for, Expr(:(=), first(it.args), sexp(last(it.args))), sexp(last(args)))
+        se"Invalid iterator given to :for"
+    Expr(:for, Expr(:(=), first(it.args), sexp(last(it.args))), sexp(arg2))
 end
+kwexpr(::Val{:for}, arg1::Expr, args...) = se"Wrong number of arguments to :for"
+kwexpr(::Val{:for}, args...) = se"Invalid iterator given to :for"
 
 # A while loop.
 # Example: {:while {pred} body}
-function kwexpr(::Val{:while}, args::V)
-    ex = Expr(:row, :(:while), args...)
-    length(args) == 2 || throw(SyntaxError("Wrong number of arguments to :while", ex))
-    Expr(:while, map(sexp, args)...)
-end
+kwexpr(::Val{:while}, arg1, arg2) = Expr(:while, sexp(arg1), sexp(arg2))
+kwexpr(::Val{:while}, args...) = se"Wrong number of arguments to :while"
 
 # A multi-branch if statement, i.e. elseif.
 # Example: {:cond {{pred1 x} body1} {{pred2 x} body2} {true body_fallback}}
 # Note that no else is inserted.
-function kwexpr(::Val{:cond}, args::V)
+function kwexpr(::Val{:cond}, args...)
     # TODO
 end
 
 # TODO: try/catch/finally, let, defun, defmacro.
-
-# Equivalent to [h | t] = list.
-peel(args::V) = args[1], args[2:end]
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,46 @@
-using JLisp: parse
+using JLisp
 using Test
 
-@testset "JLisp" begin
-    ex = parse("{max 1 2}")
-    @test ex == [:(max(1, 2))]
-    ex = parse("{+ 1 2}")
-    @test ex == [:(1 + 2)]
-    ex = parse("{}")
+macro test_sexp(ex, expected)
+    quote
+        let ex = JLisp.sexp($(QuoteNode(ex)))
+            @test ex == $expected
+        end
+    end
+end
+
+@testset "jlisp" begin
+    @testset "S-expression parsing" begin
+        @test_sexp {max 1 2} Expr(:call, :max, 1, 2)
+        @test_sexp {+ 1 2} Expr(:call, :+, 1, 2)
+        @test_sexp {❜> 1 2} Expr(:call, :>, 1, 2)
+        @test_sexp {:def x 1} Expr(:(=), :x, 1)
+        @test_sexp {:if true 1 2} Expr(:if, :true, 1, 2)
+        @test_sexp {:for {x xs} x} Expr(:for, Expr(:(=), :x, :xs), :x)
+        @test_sexp {:while true fun} Expr(:while, :true, :fun)
+    end
+
+    # @testset "String parsing" begin
+    #     code = """
+    #         {:def x 1}
+    #         {:while {❜< x 10}
+    #             {def x {+ x 1}}}
+    #         {:if {❜!= x 10}
+    #             {error "Something is broken!"}}
+    #         """
+    #     ex = JLisp.parse(code)
+    #     @test ex == Expr(:block, Any[
+    #         Expr(:(=), :x, 1),
+    #         Expr(
+    #             :while,
+    #             Expr(:call, :<, :x, 10),
+    #             Expr(:(=), :x, Expr(:call, :+, :x, 1)),
+    #         ),
+    #         Expr(
+    #             :if,
+    #             Expr(:call, :!=, :x, 10),
+    #             Expr(:call, :error, "Something is broken!"),
+    #         ),
+    #     ]
+    # end
 end
